94faa1db1e208e63057f51c0409cdd448f57468b
MilevAngel4 <anio7@mail.bg>
2017-11-19 13:16:46 +0200

    C# Homework week8

---
+++ STD_3a_AngelMilev_1701681083_H8/CircleArea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int radius = rnd.Next(33, 187);
+
+            double pi = Math.PI;
+
+            double area = pi * (radius * radius);
+            Console.WriteLine("The circle random height {0}", radius);
+            Console.WriteLine("the circle are {0}", Math.Round(area, 2));
+
+
+
+        }
+    }
+}
